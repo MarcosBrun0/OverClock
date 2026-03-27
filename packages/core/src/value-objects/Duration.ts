2f82f1308517693c44452ca6afea453a2f176e55
MB <mb@dev.local>
2026-03-26 23:51:22 -0300

    duration formating string

--- packages/core/src/value-objects/Duration.ts
+++ packages/core/src/value-objects/Duration.ts
@@ -6,14 +6,20 @@ export class Duration {
 
 
   static fromSeconds(s: number): Duration {
-    if (s < 0) throw new Error("Duration cant be negative")
+    if (s < 0) throw new Error("Duration can't be negative")
     return new Duration(s)
   }
-  static zero() { return new Duration(0); }
+
+  static zero() {
+    return new Duration(0)
+  }
   get minutes() { return Math.floor(this.seconds / 60); }
-  get hours() { return Math.floor(this.seconds / 3600) }
+  get hours() { return Math.floor(this.seconds / 3600); }
+
   format(): string {
-    const h = this.hours
-    const m = this.minutes
+    const h = this.hours;
+    const m = this.minutes % 60; //isola minutos de horas 
+    if (h > 0) { return `${h}h ${m}min` }
+    return `${m}min`
   }
 }
